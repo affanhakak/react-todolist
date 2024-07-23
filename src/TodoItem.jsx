66579c5c4d36f6b27780375249d25f36ba60555c
Affan Hakak <affanshowkat@gmail.com>
2024-07-23 17:24:09 +0530

    added local storage to save our state

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/prop-types */
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
-import CommentIcon from "@mui/icons-material/Comment";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function TodoItem({ todo, removeTodo, toggleTodo }) {
   const labelId = `checkbox-list-label-${todo.id}`;
@@ -13,7 +14,7 @@ export default function TodoItem({ todo, removeTodo, toggleTodo }) {
       <ListItem
         secondaryAction={
           <IconButton edge="end" aria-label="comments" onClick={removeTodo}>
-            <CommentIcon />
+            <DeleteIcon />
           </IconButton>
         }
         disablePadding
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,22 @@
 import List from "@mui/material/List";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
 
-const initialTodos = [
-  { id: 1, text: "walk the dog", completed: false },
-  { id: 2, text: "walk the cat", completed: false },
-  { id: 3, text: "walk the rabbit", completed: true },
-  { id: 4, text: "walk the chickens", completed: false },
-];
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) {
+    return [];
+  } else {
+    return data;
+  }
+};
 
 export default function TodoList() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState(getInitialData);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const removeTodo = (id) => {
     setTodos((prevTodos) => {
       return prevTodos.filter((t) => t.id !== id);
@@ -27,6 +33,14 @@ export default function TodoList() {
       });
     });
   };
+  const addTodo = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { text: text, id: crypto.randomUUID(), completed: false },
+      ];
+    });
+  };
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => (
@@ -37,6 +51,7 @@ export default function TodoList() {
           toggleTodo={() => toggleTodo(todo.id)}
         />
       ))}
+      <TodoForm addTodo={addTodo} />
     </List>
   );
 }
